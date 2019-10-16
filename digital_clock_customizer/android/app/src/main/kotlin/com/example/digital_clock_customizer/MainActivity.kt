8bc0d72e195ca37858c5fe491bf8c0bb252b1e50
Mary Xia <maryxia@google.com>
2019-10-16 16:57:33 -0700

    Create digital clock customizer

---
+++ digital_clock_customizer/android/app/src/main/kotlin/com/example/digital_clock_customizer/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.digital_clock_customizer
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
