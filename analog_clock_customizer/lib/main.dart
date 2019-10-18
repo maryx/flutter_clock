f669d911b640c972ce02ce5d52ea681fe47ba88b
Mary <maryx@users.noreply.github.com>
2019-10-18 15:01:27 -0700

    Remove tests folders and comments (#9)
    * Remove tests folders and comments
    
    * add TODOs

--- analog_clock_customizer/lib/main.dart
+++ analog_clock_customizer/lib/main.dart
@@ -1,8 +1,12 @@
+// TODO: If you want to use your own clock package, import it here and
+// in pubspec.yaml
 import 'package:analog_clock/analog_clock.dart';
 import 'package:clock_customizer/clock_customizer.dart';
 import 'package:flutter/material.dart';
 import 'package:model/model.dart';
 
+// TODO: If you want to use your own clock, replace `AnalogClock(model)`
+// with your clock widget.
 void main() => runApp(MaterialApp(
       title: 'Analog Clock with Customizer',
       home: Scaffold(
