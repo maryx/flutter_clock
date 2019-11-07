68b25669d59f44bf3282e09fa6d907dec460471e
Mary <maryx@users.noreply.github.com>
2019-11-06 16:11:37 -0800

    Move stuff to flutter_clock_helper and *_clock (#18)
    * Move stuff to flutter_clock_helper and each clock
    
    * delete unused digital_clock
    
    * move *_clock_customizer to *_clock
    
    * Move *_customizer to *_clock
    
    * Update pubspec locks
    
    * cleanup

---
+++ analog_clock/lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter_clock_helper/clock_customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'analog_clock.dart';
+
+// TODO: If you want to use your own clock, replace
+// `AnalogClock(model: model, weatherModel: weatherModel)` with your clock widget.
+void main() {
+  /// A temporary measure until TargetPlatform supports macOS.
+  if (Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS. https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+  runApp(ClockCustomizer((ClockModel model, WeatherModel weatherModel) => AnalogClock(model, weatherModel)));
+}
---
+++ digital_clock/lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter_clock_helper/clock_customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'digital_clock.dart';
+
+// TODO: If you want to use your own clock, replace `DigitalClock(model)`
+// with your clock widget.
+void main() {
+  /// A temporary measure until TargetPlatform supports macOS.
+  if (Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS. https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+  runApp(ClockCustomizer((ClockModel model, WeatherModel weatherModel) =>
+      DigitalClock(model, weatherModel)));
+}
